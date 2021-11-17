cf8c9dba9db82ebd71bcfaf9392b8cb4d8c9e745
UxerDevCoding <omarheviaarbana@gmail.com>
2021-11-17 12:47:11 +0100

    addParticipant function and renderParticipantList added

---
+++ src/scripts/app.js
@@ -0,0 +1,34 @@
+let inputNewParticipant = document.getElementById("input-new-participant");
+let btnNewParticipant = document.getElementById("btn-new-participant");
+let participantsContainer = document.getElementById("participants-container");
+let listofParticipants = [];
+
+btnNewParticipant.addEventListener('click', addParticipant);
+
+function addParticipant() {
+    if(inputNewParticipant.value != "") {
+        listofParticipants.push(inputNewParticipant.value);
+        renderParticipantsList();
+    }
+    inputNewParticipant.placeholder = '¡Añade participante!';
+    inputNewParticipant.value = '';
+}
+
+function renderParticipantsList() {
+    participantsContainer.innerHTML = '<ul id="ul-participants"></ul>';
+
+    listofParticipants.forEach(participant => {
+        let pullParticipant = document.getElementById("ul-participants");
+        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
+    })
+}
+
+let btnDelete = document.getElementById("btn-delete");
+let btnEdit = document.getElementById("btn-edit");
+
+btnDelete.addEventListener('click', deleteParticipant);
+
+function deleteParticipant(id) {
+    document.getElementById(id).remove();
+}
+
