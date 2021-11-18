63610dedc41b2834c303abaf16703a51bd7771b8
elegarmi <elegarmi@gmail.com>
2021-11-18 22:10:31 +0100

    Improve selectedWinner function, insert addParticipant with enter key, update layout styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,8 +5,21 @@ let selectedList = document.getElementById("section__to-play--selected-list");
 let btnPlay = document.getElementById("btn-play");
 let listofParticipants = [];
 
-btnNewParticipant.addEventListener('click', addParticipant);
+function renderParticipantsList() {
+    participantsContainer.innerHTML = '<ul id="ul-participants"></ul>';
+    selectedList.innerHTML = '<ul id="ul-selected"></ul>';
 
+    listofParticipants.forEach(participant => {
+        let pullParticipant = document.getElementById("ul-participants");
+        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete-${participant}"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
+
+        eventToDelete(participant);
+    })
+}
+
+renderParticipantsList();
+
+btnNewParticipant.addEventListener('click', addParticipant);
 
 function addParticipant() {
     if(inputNewParticipant.value != "") {
@@ -17,33 +30,38 @@ function addParticipant() {
     if (inputNewParticipant.value = '') {
         inputNewParticipant.placeholder = '¡Añade participante!';
     }
-}       
+}      
 
-function renderParticipantsList() {
-    participantsContainer.innerHTML = '<ul id="ul-participants"></ul>';
-
-    listofParticipants.forEach(participant => {
-        let pullParticipant = document.getElementById("ul-participants");
-        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete-${participant}"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
-
-        eventToDelete(participant);
-    })
-}
+inputNewParticipant.addEventListener('keyup', function (event) {
+    if(event.key === 'Enter') {
+        addParticipant();
+    }
+}) ;
 
 btnPlay.addEventListener('click', selectedWinner);
 
+function tubeFocused() {
+    document.getElementById("tube-image").focus();
+}
+
 function selectedWinner () {
-    let winner = Math.floor(Math.random() * listofParticipants.length);
-    let winnerName = listofParticipants[winner];
-    selectedList.innerHTML += `<li>${winnerName}</li>`
-    listofParticipants.splice(winner, 1);
-    //If para tratar inhabilitar el juego
+    tubeFocused();    
+    
+    if (listofParticipants.length > 0) {
+        let winner = Math.floor(Math.random() * listofParticipants.length);
+        let winnerName = listofParticipants[winner];
+        let ulSelected = document.getElementById("ul-selected");
+
+        ulSelected.innerHTML += `<li>${winnerName}</li>`
+        listofParticipants.splice(winner, 1);
+    }
+    
     if (listofParticipants.length === 0) {
+        console.log("no hay mas participantes");
     }
 }
 
-function deleteParticipant()
-{
+function deleteParticipant() {
     console.log('deleted');
 }
 
@@ -51,6 +69,5 @@ function eventToDelete(player) {
     let btnDelete = document.getElementById(`btn-delete-${player}`);
         btnDelete.addEventListener('click', () => {
             console.log(player);
-            // deleteParticipant();
         })
 }
