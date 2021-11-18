682d6c0daecf3c25be9ff5a24706ff532b46e61c
UxerDevCoding <omarheviaarbana@gmail.com>
2021-11-18 08:55:11 +0100

    delete buttonn different options on app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -19,16 +19,29 @@ function renderParticipantsList() {
 
     listofParticipants.forEach(participant => {
         let pullParticipant = document.getElementById("ul-participants");
-        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
+        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete" onclick="deleteParticipant('${participant.replace(/\s|["']/g, '')}')"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
     })
 }
+
+renderParticipantsList()
 
 let btnDelete = document.getElementById("btn-delete");
 let btnEdit = document.getElementById("btn-edit");
 
-btnDelete.addEventListener('click', deleteParticipant);
+btnDelete.addEventListener('click', deleteParticipant(id));
 
-function deleteParticipant(id) {
-    document.getElementById(id).remove();
-}
+// function deleteParticipant() {
+//     //$(this).closest("li").remove(); 
+//     alert("Bo, qué pacha");
+// }
+
+// function deleteParticipant(id) {
+//     document.getElementById(id).remove();
+//     // renderParticipantsList();
+// }
+
+// $("ul").on("click", "button", function(e) {
+//     e.preventDefault();
+//     $(this).parent().remove();
+// });
 
