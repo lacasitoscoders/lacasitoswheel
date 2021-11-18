a3153121759f025f88510d0177fa7c3fffc728db
UxerDevCoding <omarheviaarbana@gmail.com>
2021-11-18 12:18:39 +0100

    function eventToDelete added and function renderParticipansList updated

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,43 +5,41 @@ let listofParticipants = [];
 
 btnNewParticipant.addEventListener('click', addParticipant);
 
+
 function addParticipant() {
     if(inputNewParticipant.value != "") {
         listofParticipants.push(inputNewParticipant.value);
         renderParticipantsList();
     }
-    inputNewParticipant.placeholder = '¡Añade participante!';
-    inputNewParticipant.value = '';
+
+    if (inputNewParticipant.value = '') {
+        inputNewParticipant.placeholder = '¡Añade participante!';
+    }
 }
+        
+        
 
 function renderParticipantsList() {
     participantsContainer.innerHTML = '<ul id="ul-participants"></ul>';
 
     listofParticipants.forEach(participant => {
         let pullParticipant = document.getElementById("ul-participants");
-        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete" onclick="deleteParticipant('${participant.replace(/\s|["']/g, '')}')"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
+        pullParticipant.innerHTML += `<li id="${participant.replace(/\s|["']/g, '')}"><span>${participant}</span><i class="fa fa-user-minus" id="btn-delete-${participant}"></i><i class="fa fa-edit" id="btn-edit"></i></li>`;
+
+        eventToDelete(participant);
     })
 }
 
-renderParticipantsList()
 
-let btnDelete = document.getElementById("btn-delete");
-let btnEdit = document.getElementById("btn-edit");
+function deleteParticipant()
+{
+    console.log('deleted');
+}
 
-btnDelete.addEventListener('click', deleteParticipant(id));
-
-// function deleteParticipant() {
-//     //$(this).closest("li").remove(); 
-//     alert("Bo, qué pacha");
-// }
-
-// function deleteParticipant(id) {
-//     document.getElementById(id).remove();
-//     // renderParticipantsList();
-// }
-
-// $("ul").on("click", "button", function(e) {
-//     e.preventDefault();
-//     $(this).parent().remove();
-// });
-
+function eventToDelete(player) {
+    let btnDelete = document.getElementById(`btn-delete-${player}`);
+        btnDelete.addEventListener('click', () => {
+            console.log(player);
+            // deleteParticipant();
+        })
+}
